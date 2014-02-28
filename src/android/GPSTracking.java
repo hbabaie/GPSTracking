05771601ff8a42e700d8e6631322f4d36d5caa3b
unknown <hosein.babaie@gmail.com>
2014-02-28 22:37:35 +0330

    commit 10

--- src/android/GPSTracking.java
+++ src/android/GPSTracking.java
@@ -49,14 +49,14 @@ public class GPSTracking extends CordovaPlugin {
 			        public void onStatusChanged(String provider, int status, Bundle extras) {}
 			    };
 				
-				lm = (LocationManager) (TelephonyManager)this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
-				try{gps_enabled=lm.isProviderEnabled(LocationManager.GPS_PROVIDER);}catch(Exception ex){}
-		        try{network_enabled=lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);}catch(Exception ex){}
-		        if(!gps_enabled && !network_enabled)
-		        	callbackContext.error("There's no active location provider on your device.");
-				 
-		        lm.requestLocationUpdates(Interval, 0, criteria, listener);
-////		        
+				lm = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
+//				try{gps_enabled=lm.isProviderEnabled(LocationManager.GPS_PROVIDER);}catch(Exception ex){}
+//		        try{network_enabled=lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);}catch(Exception ex){}
+//		        if(!gps_enabled && !network_enabled)
+//		        	callbackContext.error("There's no active location provider on your device.");
+//				 
+//		        lm.requestLocationUpdates(Interval, 0, criteria, listener);
+		        
 //				this.cordova.getThreadPool().execute(new Runnable() {
 //				    public void run() {
 //				        // Main Code goes here
