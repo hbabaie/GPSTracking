6a540acb8badfd7c6c3faedfcb4b440460c36df5
unknown <hosein.babaie@gmail.com>
2014-02-28 22:48:17 +0330

    commit 10

--- src/android/GPSTracking.java
+++ src/android/GPSTracking.java
@@ -52,11 +52,11 @@ public class GPSTracking extends CordovaPlugin {
 				lm = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
 				try { gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER); }
 				catch(Exception ex) {}
-//		        try { network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER); }
-//		        catch(Exception ex) {}
-//		        if(!gps_enabled && !network_enabled)
-//		        	callbackContext.error("There's no active location provider on your device.");
-//				 
+		        try { network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER); }
+		        catch(Exception ex) {}
+		        if(!gps_enabled && !network_enabled)
+		        	callbackContext.error("There's no active location provider on your device.");
+		        
 //		        lm.requestLocationUpdates(Interval, 0, criteria, listener);
 //		        
 //				this.cordova.getThreadPool().execute(new Runnable() {
