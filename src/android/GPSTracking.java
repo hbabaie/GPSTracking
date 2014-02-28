7e600c2ea0ad0ee1532c12a3114a8892647abc61
unknown <hosein.babaie@gmail.com>
2014-02-28 21:05:27 +0330

    commit 10

--- src/android/GPSTracking.java
+++ src/android/GPSTracking.java
@@ -7,8 +7,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 //import org.apache.http.client;
-//import org.apache.http.client.methods;
+import org.apache.http.client.methods.HttpGet;
 //import org.apache.http.impl.client;
+
+//HttpClient
+//DefaultHttpClient
+//ResponseHandler
+//BasicResponseHandler
 
 import android.location.Location;
 import android.location.LocationListener;
